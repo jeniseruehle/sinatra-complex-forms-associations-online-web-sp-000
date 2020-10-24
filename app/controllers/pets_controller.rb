94ce2d161ad1d6bc573b7d1c794047376e48ff0d
Jenise Ruehle <jela07@gmail.com>
2020-10-24 18:43:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,6 +31,7 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
